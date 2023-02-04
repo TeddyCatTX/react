77936668340b14fd897aaf563574f7319bff6bdd
TeddyCatTX <kay.dmz@gmail.com>
2023-02-04 17:26:29 -0600

    Install Tailwind CSS for React - React Tutorial 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee'
 import { useState } from 'react';
 
@@ -6,7 +6,10 @@ function App() {
   const [role, setRole] = useState('dev')
   const showEmployees = true
   return (
-    <div className="App">
+    <div className="App bg-red-300">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       {showEmployees ?  
         <>
           <input 
